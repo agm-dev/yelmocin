48831dfa23d3e4bd4c7d15758af610cae842c8ae
Adrian Gonzalo <gonzahh@gmail.com>
2022-05-23 23:57:40 +0200

    finished 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { writeFileSync } from "fs";
-import { getCinemaFilmingListingsForDate } from "./utils/film-listings";
-
-getCinemaFilmingListingsForDate("Rivas H2O", new Date())
-  .then(data => writeFileSync("data/rivas-h2o.json", JSON.stringify(data, null, 2)))
+export {
+  getCityFilmListings,
+  getCityFilmingListingsForDate,
+  getCinemaFilmingListingsForDate,
+} from "./utils/film-listings"
+export {
+  getCities,
+  getCityCinemas,
+  getCinemasOnRealTime,
+} from "./utils/cities"
